a42a04aeb9468be4e1d92119c38a76c0daab2bca
Mourad Mourad <mourad272@icloud.com>
2024-06-17 22:48:58 -0500

    sql schema creation, table creation, insert seed data." " exit exit() \q qiut quit .exit

--- sql-basics/create_schema.sql
+++ sql-basics/create_schema.sql
@@ -2,8 +2,32 @@
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
   id           serial PRIMARY KEY,
-  first_name   varchar(255) NODT NULL,
+  first_name   varchar(255) NOT NULL,
   last_name    varchar(255) NOT NULL,
   birthdate    date NOT NULL,
   address_id   integer
+);
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id           serial PRIMARY KEY,
+  line_1       varchar(255),
+  city         varchar(20),
+  state        character(20),
+  zipcode      integer
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id           serial PRIMARY KEY,
+  name         varchar(50),
+  credits      integer
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id           serial PRIMARY KEY,
+  student_id   integer,
+  class_id     integer,
+  grade        character(1)
 );
